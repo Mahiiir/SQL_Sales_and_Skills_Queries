34014ceec3e3b423c9c6ecfa83a5276a6582147d
Mahir <97622946+Mahiiir@users.noreply.github.com>
2024-09-29 16:59:52 -0400

    Create candidate_skills_query.sql

---
+++ candidate_skills_query.sql
@@ -0,0 +1,28 @@
+Question - 3:
+
+CREATE TABLE Applications (
+    candidate_id INTEGER,
+    skills VARCHAR
+);
+
+-- Inserting data into the Applications table
+INSERT INTO Applications (candidate_id, skills) VALUES
+(101, 'Power BI'),
+(101, 'Python'),
+(101, 'SQL'),
+(102, 'Tableau'),
+(102, 'SQL'),
+(108, 'Python'),
+(108, 'SQL'),
+(108, 'Power BI'),
+(104, 'Python'),
+(104, 'Excel');
+
+WITH Rskills AS(
+	SELECT candidate_id, skills
+	FROM Applications
+	WHERE skills IN ('Python', 'SQL', 'Power BI'))
+SELECT candidate_id, COUNT(skills)
+FROM Rskills
+GROUP BY candidate_id
+HAVING COUNT(skills) = 3;
